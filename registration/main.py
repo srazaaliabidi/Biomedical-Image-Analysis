20079ca831282447a8eb1ef5157de6a52144194e
William Lew <WilliamMilesLew@gmail.com>
2021-03-30 18:19:03 -0700

    added: basic project files

--- registration/main.py
+++ registration/main.py
@@ -6,15 +6,42 @@ import SimpleITK as sitk
 import sys
 import os
 
-# the directory should contain .dcm files
-directory_path = "data/MIDRC-RICORD-1A/MIDRC-RICORD-1A-419639-000082/1.2.826.0.1.3680043.10.474.419639.312580455409613733097488204614/08-02-2002/1.2.826.0.1.3680043.10.474.419639.108518937868403894887894311320"
+def command_iteration(method):
+  print(f"{method.GetOptimizerIteration():3} = {method.GetMetricValue():10.5f} : {method.GetOptimizerPosition()}")
 
-reader = sitk.ImageSeriesReader()
-dicom_names = reader.GetGDCMSeriesFileNames(directory_path)
-reader.SetFileNames(dicom_names)
+def segmentation(fixed_file_name, moving_file_name, output_file_name):
+  fixed = sitk.ReadImage(fixed_file_name, sitk.sitkFloat32)
+  moving = sitk.ReadImage(moving_file_name, sitk.sitkFloat32)
 
-image = reader.Execute()
+  R = sitk.ImageRegistrationMethod()
+  R.SetMetricAsMeanSquares()
+  R.SetOptimizerAsRegularStepGradientDescent(4.0, .01, 200)
+  R.SetInitialTransform(sitk.TranslationTransform(fixed.GetDimension()))
+  R.SetInterpolator(sitk.sitkLinear)
 
-size = image.GetSize()
-print("Image size:", size[0], size[1], size[2])
-sitk.Show(image, "Dicom Series")
+  R.AddCommand(sitk.sitkIterationEvent, lambda: command_iteration(R))
+
+  outTx = R.Execute(fixed, moving)
+
+  print("-------")
+  print(outTx)
+  print(f"Optimizer stop condition: {R.GetOptimizerStopConditionDescription()}")
+  print(f" Iteration: {R.GetOptimizerIteration()}")
+  print(f" Metric value: {R.GetMetricValue()}")
+
+  sitk.WriteTransform(outTx, output_file_name)
+
+  if ("SITK_NOSHOW" not in os.environ):
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(fixed)
+    resampler.SetInterpolator(sitk.sitkLinear)
+    resampler.SetDefaultPixelValue(100)
+    resampler.SetTransform(outTx)
+
+    out = resampler.Execute(moving)
+    simg1 = sitk.Cast(sitk.RescaleIntensity(fixed), sitk.sitkUInt8)
+    simg2 = sitk.Cast(sitk.RescaleIntensity(out), sitk.sitkUInt8)
+    cimg = sitk.Compose(simg1, simg2, simg1 // 2. + simg2 // 2.)
+    sitk.Show(cimg, "ImageRegistration1 Composition")
+
+segmentation("data/processed/54.png", "data/processed/54.png", "data/results/output.txt")
--- tests/ProcessFiles.py
+++ tests/ProcessFiles.py
@@ -6,13 +6,15 @@ import SimpleITK as sitk
 import sys
 import os
 
-# the directory should contain .dcm files
 directory_path = "data/raw/MIDRC-RICORD-1A/MIDRC-RICORD-1A-419639-000082/1.2.826.0.1.3680043.10.474.419639.312580455409613733097488204614/08-02-2002/1.2.826.0.1.3680043.10.474.419639.108518937868403894887894311320"
 
 reader = sitk.ImageSeriesReader()
 dicom_names = reader.GetGDCMSeriesFileNames(directory_path)
 
-image = sitk.ReadImage(dicom_names[50])
-image = sitk.IntensityWindowing(image, -1000, 1000, 0, 255)
-image = sitk.Cast(image, sitk.sitkUInt8)
-sitk.WriteImage(image, "output.png")
+counter = 0
+for dicom_name in dicom_names:
+  image = sitk.ReadImage(dicom_name)
+  image = sitk.IntensityWindowing(image, -1000, 1000, 0, 255)
+  image = sitk.Cast(image, sitk.sitkUInt8)
+  sitk.WriteImage(image, "data/processed/" + str(counter) + ".png")
+  counter += 1
